31beb63e91f134d8ba77c1df5b9e92fc2b35386d
Kadir Berk Yağar <ykadirberk2@gmail.com>
2024-01-13 23:13:17 +0300

    added timer to makemove of cpuplayer

--- AIProject3/src/CpuPlayer/CpuPlayer.cpp
+++ AIProject3/src/CpuPlayer/CpuPlayer.cpp
@@ -2,6 +2,7 @@
 
 #include <sstream>
 #include <iostream>
+#include <chrono>
 
 CpuPlayer::CpuPlayer() {
 	std::stringstream ss;
@@ -10,12 +11,15 @@ CpuPlayer::CpuPlayer() {
 	m_PlayerCount++;
 }
 Move CpuPlayer::MakeMove(std::shared_ptr<StateHandler> handler, std::vector<int>& t_current) {
-
+	auto timer_start = std::chrono::high_resolution_clock::now();
+	
 	handler->GenerateNewPlays(t_current);
-
 	Move t = handler->ChoosePlay();
+	
+	auto timer_end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> duration = timer_end - timer_start;
 
-	std::cout << "(" << t.pos_y << ", " << t.pos_x << ") -> " << Move::CastKey(t.key) << std::endl;
+	std::cout << "(" << t.pos_y << ", " << t.pos_x << ") -> " << Move::CastKey(t.key) << ", took " << duration.count() << " milliseconds to choose." << std::endl;
 	std::cin.get();
 
 	return t;
